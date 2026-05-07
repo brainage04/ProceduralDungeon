5490f3249a84d6fc4d47f02da9d9e074952a8964
brainage04 <thomasmcmah21@gmail.com>
2026-05-07 10:06:43 +1000

    remove example stuff

--- src/main/java/com/github/brainage04/procedural_dungeon/command/core/ModCommands.java
+++ src/main/java/com/github/brainage04/procedural_dungeon/command/core/ModCommands.java
@@ -1,6 +1,5 @@
 package com.github.brainage04.procedural_dungeon.command.core;
 
-import com.github.brainage04.procedural_dungeon.command.ExampleCommand;
 import com.github.brainage04.procedural_dungeon.command.GenerateDungeonCommand;
 import com.github.brainage04.procedural_dungeon.command.TestDungeonVariantsCommand;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -8,8 +7,6 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 public class ModCommands {
     public static void initialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            ExampleCommand.initialize(dispatcher);
-
             GenerateDungeonCommand.initialize(dispatcher);
 
             TestDungeonVariantsCommand.initialize(dispatcher);
